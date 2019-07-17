d1f20076427fff78d5eb4e8781c4a102b24ae67d
Rodrigo Lousada <rodrigo.m.lousada@gmail.com>
2019-07-17 14:27:56 +0100

    Foreign Keys, Tutorial Categories and Series

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tutorial
+from .models import Tutorial, TutorialSeries, TutorialCategory
 from tinymce.widgets import TinyMCE
 from django.db import models
 
@@ -11,12 +11,16 @@ class TutorialAdmin(admin.ModelAdmin):
     #            "tutorial_content"]
 
     fieldsets = [
-            ("Title/date", {"fields": ["tutorial_title", "tutorial_published"]}),
-            ("Content", {"fields": ["tutorial_content"]})
+        ("Title/date", {'fields': ["tutorial_title", "tutorial_published"]}),
+        ("URL", {'fields': ["tutorial_slug"]}),
+        ("Series", {'fields': ["tutorial_series"]}),
+        ("Content", {"fields": ["tutorial_content"]})
     ]
 
     formfield_overrides = {
         models.TextField: {'widget': TinyMCE()},
     }
 
+admin.site.register(TutorialSeries)
+admin.site.register(TutorialCategory)
 admin.site.register(Tutorial, TutorialAdmin)
